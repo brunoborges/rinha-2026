ecdaf2ac78f2be33be03dfdb45104a2e36fab4a8
Bruno Borges <brborges@microsoft.com>
2026-06-01 10:18:48 -0400

    initial commit

---
+++ src/main/java/io/github/brunoborges/rinha2026/App.java
@@ -0,0 +1,10 @@
+package io.github.brunoborges.rinha2026;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
